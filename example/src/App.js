123324b86f2a159f76a24b599912943c8cea4eb8
Quinn <maitrucquynh111@gmail.com>
2019-08-09 11:22:55 +0700

    update

--- example/src/App.js
+++ example/src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react'
 
 import ExampleComponent from 'create-react-library-masspa'
-
+import imageAvatar from './img/avatar-image.png'
+import imageBackground from './img/background.jpg'
+import styles from './index.css'
 export default class App extends Component {
   render () {
     return (
-      <div>
-        <ExampleComponent text='AloAlo' />
+      <div className='root'>
+        <ExampleComponent imageAvatar={imageAvatar} imageBackground={imageBackground}/>
       </div>
     )
   }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
-
+import './reset.css'
 import styles from './styles.css'
 
 export default class ExampleComponent extends Component {
@@ -10,13 +10,31 @@ export default class ExampleComponent extends Component {
 
   render() {
     const {
-      text
+      imageAvatar,
+      imageBackground
     } = this.props
 
     return (
-      <div className={styles.test}>
-        Example Component: {text}
-      </div>
+      // <div className={styles.container} style={{"background": `url(${imageBackground})`}}>
+        <div className={styles.loginBox}>
+            <img className={styles.imgAvatar} src={imageAvatar} alt="Avatar"/>
+            <h1 className={styles.titleForm}>Login Form</h1>
+            <form action="#">
+                <label className={styles.labelInput} for="username-input">Username</label>
+                <input className={styles.inputField} type="text" id="username-input" placeholder="Enter your username"/>
+
+                <label className={styles.labelInput} for="password-input">Password</label>
+                <input className={styles.inputField} type="password" id="password-input" placeholder="Enter your password"/>
+                <input className={styles.btnSubmit} type="submit" value="Login" />
+                <a href="#">Forget your password</a>
+                <a href="#">Not have account</a>
+            </form>
+
+          </div>
+        // </div>
+      // <div className={styles.test}>
+      //   Example Component: {text}
+      // </div>
     )
   }
 }
